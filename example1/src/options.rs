bd372ea114c0c3022346ff89996d93b1393aeea5
William C. Canin <william.costa.canin@gmail.com>
2024-09-02 15:24:01 -0300

    Removing local constants to assign in .env file

--- example1/src/options.rs
+++ example1/src/options.rs
@@ -1,25 +1,20 @@
-use crate::database::models::ConnectionData;
+use crate::{database::models::ConnectionData, string_to_bool};
 use dotenv::dotenv;
 use std::env;
 
-// Url de conexão local (localhost).
-pub const DEVELOPMENT_URL: &str = "redis://127.0.0.1:6379/";
-
 // Dados de conexão remota.
+// Nota: Se o "PRODUCTION=false" no arquivo .env, o TLS será desabilitado automaticamente, ou seja,
+// mesmo que "TLS=true" no arquivo .env, ele não irá surgir efeito.
 pub fn connection_data() -> ConnectionData {
   // Carrega as variáveis do arquivo .env
   dotenv().ok();
-  // Set as variáveis do arquivo .env
   ConnectionData {
-    development: false,
+    production: string_to_bool!(env::var("PRODUCTION").unwrap()),
+    tls: string_to_bool!(env::var("TLS").unwrap()),
+    development_url: env::var("DEVELOPMENT_URL").unwrap(),
     username: env::var("REDIS_USERNAME").unwrap(),
     hostname: env::var("REDIS_HOSTNAME").unwrap(),
     password: env::var("REDIS_PASSWORD").unwrap(),
     port: env::var("REDIS_PORT").unwrap(),
   }
 }
-
-// Define se a conexão será em modo TLS ou não.
-// Nota: Se o "development: true" O TLS será desabilitado automaticamente, ou seja,
-// mesmo que "TLS = true", não irá surgir efeito.
-pub const TLS: bool = true;
