5cedffe5c0c89a702274f5df0e6d9be0479210cb
William C. Canin <william.costa.canin@gmail.com>
2024-09-02 15:25:02 -0300

    Adding development_url, production and tls attribute.

--- example1/src/database/models.rs
+++ example1/src/database/models.rs
@@ -1,5 +1,7 @@
 pub struct ConnectionData {
-  pub development: bool,
+  pub production: bool,
+  pub tls: bool,
+  pub development_url: String,
   pub username: String,
   pub password: String,
   pub hostname: String,
